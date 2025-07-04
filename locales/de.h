dd4dd14b8e1a99752fcce0afeca4fe22a38b8c13
Philipp Johann Fritz <philipp.j.fritz@gmail.com>
2025-07-04 02:39:15 +0200

    Add locale support

---
+++ locales/de.h
@@ -0,0 +1,29 @@
+#include "locales.h"
+
+static const char* const WEEKDAYS_DE[7] = {"So","Mo","Di","Mi","Do","Fr","Sa"};
+
+const LocaleStrings LOCALE_DE = {
+    "de",
+    "Deutsch",
+    "Aura Einstellungen",
+    "Helligkeit:",
+    "Ort:",
+    "Ort",
+    "In \xC2\xB0F:",
+    "24 Std:",
+    "WLAN zur\xC3\xBCcksetzen",
+    "Schlie\xC3\x9Fen",
+    "Ort \xC3\xA4ndern",
+    "Stadt:",
+    "Suchergebnisse",
+    "Speichern",
+    "Abbrechen",
+    "7-TAGE VORHERSAGE",
+    "STUNDEN-VORHERSAGE",
+    "Gef\xC3\xBChlt",
+    "Jetzt",
+    "Heute",
+    "Sprache:",
+    { WEEKDAYS_DE[0], WEEKDAYS_DE[1], WEEKDAYS_DE[2], WEEKDAYS_DE[3], WEEKDAYS_DE[4], WEEKDAYS_DE[5], WEEKDAYS_DE[6] }
+};
+
